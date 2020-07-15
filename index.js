7e762035fbeada8833a27874050b80a8860e02f5
Philip Grabenhorst <phlip007@gmail.com>
2020-07-15 13:59:45 -0700

    Initial commit (1.0)

---
+++ index.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+require('./parse_tools.js')();
+
+// NOTE: See contents of parse_tools.js,
+//  all methods and properties are now available in this file
+
+// read teh contents of Add.asm and print to console
+
+
+/** Function to turn text from a file into a simple array of lines
+ * 
+ * INTERFACE
+ * 
+ * EXPECTS: A file with \n and \r demarcations
+ * RETURNS: array of lines, free from escaped formatting
+ */
+
+const cleanFile = function(file) {
+    return file.map((line) => {
+        line = line.split('\r');
+        return line[0];
+    })
+} 
+
+
+const assembleFile = function(file_name) {
+    // Read the file, separated by line 
+    let f = fs.readFileSync('./' + file_name, 'utf8').split('\n');
+
+    // create a clean version, parsing jump symbols
+    // f = removeWhiteSpace(
+    //         parseJumpSymbols(
+    //             cleanFile(f)
+    //         )
+    //     );
+
+
+    f = cleanFile(f);
+    console.log("Cleaned file");
+
+    f = parseJumpSymbols(f);
+    console.log("Parsed Jump symbols.");
+
+    f = removeWhiteSpace(f);
+    console.log("Removed White Space");
+
+    let assembled_code = parseAllCommands(f);
+    console.log("Assembled code");
+
+
+    return assembled_code;
+}
+
+
+
+
+// MAIN
+
+if (process.argv.length == 2) {
+    console.log("Please specify a filename in the fashion 'filename.asm' to assemble it.")
+}
+
+process.argv.forEach(arg => {
+    // skip 'node' argument
+    if (arg.includes("node")) {
+        return;
+    } else if (arg.includes('index.js')) {
+        return;
+    } else {
+        try {
+            // where the magic happens
+            let asm = assembleFile(arg);
+            let new_file_name = './' + arg.split('.')[0] + '.hack';
+            fs.writeFileSync('./' + new_file_name, "");
+
+            // TMP FILE
+            let tmp = "";
+            asm.forEach((line) => {
+                tmp = tmp + ('./' + new_file_name, line+"\n");
+            })
+             
+            // write the file
+            fs.writeFileSync('./' + new_file_name, tmp);
+            console.log("Finished writing to file.")
+        } catch (err) {
+            console.log(err);
+            throw new Error(`Sorry, ${arg} is not in this directory.`)
+        }
+    }
+})
---
+++ parse_tools.js
@@ -0,0 +1,317 @@
+
+const parseTools = function() {
+    /** Table of values
+ * 
+ * STRUCTURE
+ * 
+ * Each of these objects represents part of the HACK specification. Each key is a HACK command or predefined value,
+ *  each value is its binary representation. Binary, in this instance, is written as a string for ease of writing.
+ */
+
+this.C_DEST_SYMBOLS = {
+    "null" : "000",
+    "M" : "001",
+    "D" : "010",
+    "MD" : "011",
+    "A" : "100",
+    "AM" : "101",
+    "AD" : "110",
+    "AMD" : "111",
+}
+
+this.C_COMP_SYMBOLS = {
+    // Begin instructions for a (first bit) = 0
+    "0" : "0101010",
+    "1" : "0111111",
+    "-1" : "0111010",
+    "D" : "0001100",
+    "A" : "0110000",
+    "!D" : "0001101",
+    "!A" : "0110001",
+    "-D" : "0001111",
+    "-A" : "0110011",
+    "D+1" : "0011111",
+    "A+1" : "0110111",
+    "D-1" : "0001110",
+    "A-1" : "0110010",
+    "D+A" : "0000010",
+    "D-A" : "0010011",
+    "A-D" : "0000111",
+    "D&A" : "0000000",
+    "D|A" : "0010101",
+    // Begin instructions for a (first bit) = 1
+    "M" : "1110000",
+    "!M" : "1110001",
+    "-M" : "1110011",
+    "M+1" : "1110111",
+    "M-1" : "1110010",
+    "D+M" : "1000010",
+    "D-M" : "1010011",
+    "M-D" : "1000111",
+    "D&M" : "1000000",
+    "D|M" : "1010101",
+
+}
+
+
+this.C_JUMP_SYMBOLS = {
+    "null" : "000",
+    "JGT" : "001",
+    "JEQ" : "010",
+    "JGE" : "011",
+    "JLT" : "100",
+    "JNE" : "101",
+    "JLE" : "110",
+    "JMP" : "111",
+}
+
+this.MEM_SYMBOLS = {
+    "SP" : 0,
+    "LCL" : 1,
+    "ARG" : 2,
+    "THIS" : 3,
+    "THAT" : 4,
+    "R0" : 0,
+    "R1" : 1,
+    "R2" : 2,
+    "R3" : 3,
+    "R4" : 4,
+    "R5" : 5,
+    "R6" : 6,
+    "R7" : 7,
+    "R8" : 8,
+    "R9" : 9,
+    "R10" : 10,
+    "R11" : 11,
+    "R12" : 12,
+    "R13" : 13,
+    "R14" : 14,
+    "R15" : 15,
+    "SCREEN" : 16384,
+    "KBD" : 24576,
+}
+
+
+
+/** Function to remove white space,
+ * INTERFACE 
+ * 
+ * Expects: an array of lines of ASM code, unedited
+ * 
+ * Returns: an array of lines of ASM code, edited
+ *  - no tabs/spaces at end of line
+
+ *  - no empty lines
+ */
+
+this.removeWhiteSpace = function (line_array) {
+
+    let comment_string = "//";
+    let spaces = [" ", "    "];
+    line_array = line_array
+    .map((line) => { // clean spaces
+        function removeSpaces(a_string) {
+            a_string = a_string.replace(" ", "");
+            if (a_string.includes(" ")) {
+                return removeSpaces(a_string);
+            } else { 
+                return a_string;
+            }
+        }
+        return removeSpaces(line);
+    })
+    .map((line) => { // clean comments
+        if (line.includes(comment_string)) {
+            line = line.split("//")[0];
+            return line;
+        } else {
+            return line;
+        }
+    })
+    .filter((line) => {
+        if (! spaces.includes(line)) {
+            return line;
+        }
+    })
+
+    return line_array;
+    // clean out comments
+}
+
+
+/** Function to parse All commands 
+ * INTERFACE
+ * 
+ * Expects: an array of parsed asm code, one command per line
+ * 
+ * Returns: an array of machine code, per HACK specification one per line
+ */
+
+
+ this.parseAllCommands = function(commands) {
+    return commands.map((command) => {
+        if (command.includes('@')) {
+            return this.parseACommand(command);
+        } else {
+            return this.parseCCommand(command);
+        }
+    })
+ }
+
+
+/** Function to parse A commands
+ * 
+ * INTERFACE
+ * 
+ * Expects: an A command, syntax @<value>
+ * 
+ * Returns: parsed machine code, per HACK specs, for said A command
+ * 
+ * TODO: Symbol handling
+ */
+
+this.parseACommand = function(a_command) {
+    let new_command = "0"; // base digit of an A command
+
+    a_command = a_command.replace("@", ""); // get just the value
+
+    if (Boolean(Number(a_command) + 1)) { // handles the A command if it is a number
+        new_command = new_command + Number(a_command).toString(base=2);
+        // the new command may not be of a width of 16
+        while(new_command.length < 16) {
+            // add trailing digits
+            new_command = "0" + new_command;
+        }
+        if (new_command.length > 16) { // makes sure the number in command is less than 2^15
+            throw new Error(`Parsed representation of command produces word witha  length of ${new_command.length}, which cannot be handled by a 16-bit system.`)
+        } else {
+            return new_command;
+        }
+
+    } else { // handles the A command if it is a symbol
+        if (Object.keys(this.MEM_SYMBOLS).includes(a_command)) {
+            return this.parseACommand(`@${this.MEM_SYMBOLS[a_command]}`)
+        } else { // find a new register for the symbol and assign it, if not already existent
+            this.MEM_SYMBOLS[a_command] = this.getOpenRegister();
+            return this.parseACommand(`@${this.MEM_SYMBOLS[a_command]}`);
+        }
+    }
+
+
+}
+
+/** Function to parse C commands
+* 
+* INTERFACE
+* 
+* Expects: an C command, syntax <dest>=<comp>;<jump>
+*   - dest is optional
+*   - jump is optional
+* 
+* Returns: a string of the corresponding machine code command 
+*/
+
+this.parseCCommand = function(c_command) {
+    let new_command = "111"; // base three digits of a A command
+
+    let contains_dest = c_command.includes('=');
+    let contains_jump = c_command.includes(';');
+
+    if (contains_dest && contains_jump) { // case for <dest>=<comp>;<jmp>
+        c_command = c_command.split("=") // returns [<dest>, <comp>;<jmp>]
+        new_command += this.C_COMP_SYMBOLS[c_command[1].split(";")[0]]; // comp
+        new_command += this.C_DEST_SYMBOLS[c_command[0]]; // dest
+        new_command += this.C_JUMP_SYMBOLS[c_command[1].split(";")[1]]; // jmp
+        return new_command;
+    } else if (contains_dest && !contains_jump) { //case for <dest>=<comp>
+        c_command = c_command.split("=") // returns [<dest>, <comp>]
+        new_command += this.C_COMP_SYMBOLS[c_command[1]]; // comp
+        new_command += this.C_DEST_SYMBOLS[c_command[0]]; // dest
+        new_command += this.C_JUMP_SYMBOLS["null"]; // jmp is NULL
+        return new_command;
+    } else if (!contains_dest && contains_jump) { // case for <comp>;<jmp>
+        c_command = c_command.split(";") // returns [<comp>;<jmp>]
+        new_command += this.C_COMP_SYMBOLS[c_command[0]]; // comp
+        new_command += this.C_DEST_SYMBOLS['null']; // destination is null
+        new_command += this.C_JUMP_SYMBOLS[c_command[1]]; // jmp
+        return new_command;
+    } else {
+        throw new Error(`The syntax of the command ${c_command} does not match the specified syntax for the HACK assembly language.`)
+    }
+}
+
+
+/** TODO: Function to hoist symbols
+ *  
+ * INTERFACE:
+ * 
+ * Expects: An array of unparsed Assembly code, possibly including SYMBOLS
+ * 
+ * Returns: An array of unparsed Assembly code, assigning any symbols a value in MEM_SYMBOLS
+ * 
+ */
+
+this.parseJumpSymbols = function(command_lines) {
+    
+    // keep track of which lines are symbol lines
+    let symbol_lines = [];
+
+    // get all symbols from lines, add to MEM_SYMBOLS
+    command_lines.forEach((line) => {
+        if (line.includes('(')) { // handles case where line is a symbol identification
+            let i1 = line.indexOf('(');
+            let i2 = line.indexOf(')');
+            symbol = line.slice(i1 + 1, i2);
+            // append index of line to symbol lines 
+            symbol_lines.push(command_lines.indexOf(line));
+            // add the symbol and its value to MEM_SYMBOLS
+            this.MEM_SYMBOLS[symbol] = command_lines.indexOf(line);
+            return;
+        } else { // normal lines
+            return;
+        }
+    });
+
+    let new_command_lines = [];
+    for (let i=0; i <command_lines.length; i++) {
+        if (symbol_lines.includes(i)) {
+            continue;
+        } else {
+            new_command_lines.push(command_lines[i]);
+        }
+    }
+
+    return new_command_lines;
+
+}
+
+
+/** Function to return first open memory address 
+ * 
+ * INTERFACE 
+ * 
+ * Expects: Nothing
+ * Returns: The first available register in Memory after 15
+ * 
+ * 
+ * 
+ */
+
+ this.getOpenRegister = function() {
+    while(i=15, i++) {
+        // condition that the register is already taken
+        if (Object.values(MEM_SYMBOLS).includes(i)) {
+            continue;
+        } else if (i == this.MEM_SYMBOLS['SCREEN']) { // condition that i is greater than the value of SCREEN, in that case you are out of memory
+            throw new Error(`Sorry, the memory address ${this.MEM_SYMBOLS['SCREEN']} is allocaed for the screen. If you have tried to assign to it, then you are out of memory.`);
+            break;
+        } else { // condition that the register is available
+            return i;
+            break;
+        }
+    }
+ }
+}
+
+
+module.exports = parseTools;
